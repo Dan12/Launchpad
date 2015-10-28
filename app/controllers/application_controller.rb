40e37fcd0cc7586bea80336cda13a33bf0dfbd61
Dan W <dantech88@yahoo.com>
2015-10-28 03:54:57 +0000

    changed layout and updated sounds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def getAssetUrl
+    render :json => {"asset_path" => ActionController::Base.helpers.asset_path("chain1/#{params["file_name"]}.mp3"), "sindex" => params["sindex"]}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "application#index"
+  
+  post "/get_asset_path" => "application#getAssetUrl"
 end
