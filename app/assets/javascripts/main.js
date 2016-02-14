8354788282bac37f4990da559723109545ab6fb3
Dan W <dantech88@yahoo.com>
2016-02-14 23:18:41 +0000

    added BasicMIDI

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,14 @@ $(document).ready(function(){
         $(".buttons").css("margin", "0");
         $(".buttons").css("margin","0 "+(($("body").innerWidth()-$(".buttons").width()-30)/2)+"px");
     });
+    
+    var player = new Player();
+    
+    BasicMIDI.init("body",player,"assets/");
 });
+
+var Player = function(){}
+
+Player.prototype.linkEditor = function(editor){
+    console.log(editor);
+}
